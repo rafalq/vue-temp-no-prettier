d59dcd0a1943b67fdd600a296179d8aa952326bd
rafalq <kruszewskirafael@gmail.com>
2022-05-27 12:12:13 +0100

    initial and final commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	mode: "jit",
+	purge: [
+		"./index.html",
+		"./src/**/*.{vue,js,ts,jsx,tsx}",
+	],
+	content: [
+		"./index.html",
+		"./src/**/*.{vue,js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
